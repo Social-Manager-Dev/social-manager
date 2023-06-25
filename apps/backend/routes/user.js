010e54379e8af88d2edb58ce00a6467a71b14109
Ebubeker Rexha <60944813+Ebubeker@users.noreply.github.com>
2023-06-25 22:14:08 +0200

    consoling

--- apps/backend/routes/user.js
+++ apps/backend/routes/user.js
@@ -1,11 +1,22 @@
 import express from "express";
+import { UserAccessKey } from "../models/UserAccessKey";
+import axios from 'axios'
 
 const router = express.Router();
 
 // Home page route.
 router.get("/get-user", function (req, res) {
   console.log(req.query.code);
-  res.send(`Access Key: ${req.query.code}`);
+  
+  axios.get(`https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=c85620d91666afed085eab5e66a0dc1f&access_token=${req.query.code}`).then((response)=>{
+    res.send(`Access Key: ${response.access_token} and expires in ${response.expires_in/86400} days`);
+  })
+
+  // const newAccessKey = new UserAccessKey({
+  //   access_key: 'hi'
+  // })
+
+  // newAccessKey.save();
 });
 
 // About page route.
