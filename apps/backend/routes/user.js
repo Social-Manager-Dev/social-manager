3f6f07c823f1d2ab5d422222e4a03646ed55a2be
Ebubeker Rexha <60944813+Ebubeker@users.noreply.github.com>
2023-06-26 21:25:50 +0200

    changes

--- apps/backend/routes/user.js
+++ apps/backend/routes/user.js
@@ -10,6 +10,8 @@ router.get("/get-user", function (req, res) {
   
   axios.get(`https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=c85620d91666afed085eab5e66a0dc1f&access_token=${req.query.code}`).then((response)=>{
     res.send(`Access Key: ${response.access_token} and expires in ${response.expires_in/86400} days`);
+  }).catch((err)=>{
+    res.send(err);
   })
 
   // const newAccessKey = new UserAccessKey({
