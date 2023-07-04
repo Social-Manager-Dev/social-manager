419104fef8a7ba29711f07c521e14013bec48e00
Ebubeker Rexha <60944813+Ebubeker@users.noreply.github.com>
2023-07-04 22:07:51 +0200

    changes

--- apps/backend/routes/user.js
+++ apps/backend/routes/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 // import UserAccessKey from "../models/UserAccessKey";
-import axios from 'axios'
+import axios from "axios";
 
 const router = express.Router();
 
@@ -8,33 +8,40 @@ const router = express.Router();
 router.get("/get-user-code", function (req, res) {
   console.log(req.query.code);
 
-  axios.post('https://api.instagram.com/oauth/access_token', {
-    client_id: '785166699786224',
-    client_secret: 'c85620d91666afed085eab5e66a0dc1f',
-    grant_type: "authorization_code",
-    redirect_uri: 'https://kullaj-social-media.onrender.com/users/get-user-token',
-    code: req.query.code,
-  })
-  
-  
+  axios.post("https://api.instagram.com/oauth/access_token", {
+    "client_id": "785166699786224",
+    "client_secret": "c85620d91666afed085eab5e66a0dc1f",
+    "grant_type": "authorization_code",
+    "redirect_uri":
+      "https://kullaj-social-media.onrender.com/users/get-user-token",
+    "code": req.query.code
+  });
+
   // const newAccessKey = new UserAccessKey({
-    //   access_key: 'hi'
-    // })
-    
-    // newAccessKey.save();
+  //   access_key: 'hi'
+  // })
+
+  // newAccessKey.save();
 });
-  
+
 router.get("/get-user-token", function (req, res) {
-    console.log(req)
-    axios.get(`https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=c85620d91666afed085eab5e66a0dc1f&access_token=${req.query.code}`).then((response)=>{
-      res.send(`Access Key: ${response.access_token} and expires in ${response.expires_in/86400} days`);
-    }).catch((err)=>{
-      res.send(err);
+  console.log(req);
+  axios
+    .get(
+      `https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=c85620d91666afed085eab5e66a0dc1f&access_token=${req.query.code}`
+    )
+    .then((response) => {
+      res.send(
+        `Access Key: ${response.access_token} and expires in ${
+          response.expires_in / 86400
+        } days`
+      );
     })
-    
-  
-})
-  
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
 // About page route.
 router.get("/get-details", function (req, res) {
   res.send("About this wiki");
